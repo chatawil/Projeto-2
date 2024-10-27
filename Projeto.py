1e0b9c3fc159cb0fbd56cd65b2a37b0cd9933215
chatawil <charlesht@al.insper.edu.br>
2024-10-27 15:55:38 -0300

    Ex 2

--- Projeto.py
+++ Projeto.py
@@ -7,3 +7,10 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
         for i in range(tamanho):
             posicoes.append([linha, coluna + i])
     return posicoes
+
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    posicoes_navio = define_posicoes(linha, coluna, orientacao, tamanho)
+    if nome_navio in frota:
+        frota[nome_navio].append(posicoes_navio)
+    else:
+        frota[nome_navio] = [posicoes_navio]
