decb8402be486da1e996306d6326b3d0540a0644
josephperl <Josephperl9@gmail.com>
2024-10-27 16:10:46 -0300

    ex3

--- Projeto.py
+++ Projeto.py
@@ -14,3 +14,11 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
         frota[nome_navio].append(posicoes_navio)
     else:
         frota[nome_navio] = [posicoes_navio]
+    return frota
+
+def faz_jogada(tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = "X"  
+    else:
+        tabuleiro[linha][coluna] = "-"  
+    return tabuleiro
